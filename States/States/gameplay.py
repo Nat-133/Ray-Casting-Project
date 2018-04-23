7dc70ffc98784eece4d338bb3eae53bcde3b68d1
Nat-133 <38531548+Nat-133@users.noreply.github.com>
2018-04-23 11:42:06 +0100

    Add files via upload

---
+++ States/States/gameplay.py
@@ -0,0 +1,8 @@
+import pygame
+import template.template
+
+class gameplay(template.state):
+	def __init__(self):
+		print("a")
+	
+a = gameplay()
---
+++ States/States/template/template.py
@@ -0,0 +1,32 @@
+
+class State:
+    """ 
+    an base class used as a marker for state classes 
+    """
+
+    def __init__(self,nextState):
+        self.nextState = nextState
+        self.quit = False
+
+    def startup(self,persistentVar):
+        ''' called when the state becomes active '''
+        raise NotImplementedError (f"No startup method for {self.__class__.__name__} state")
+
+    def exit(self):
+        ''' called when the state is switched from this state '''
+        raise NotImplementedError (f"No exit method for {self.__class__.__name__} state")
+        
+        persistentVar = ["anything","anything else"]
+        return persistentVar
+        
+    def draw(self,screen):
+        ''' draws all the necessary things to the screen '''
+        raise NotImplementedError (f"No draw method for {self.__class__.__name__} state")
+
+    def update(self):
+        ''' provides game logic '''
+        raise NotImplementedError (f"No update method for {self.__class__.__name__} state")
+
+    def getEvent(self,event):
+        '''handles a single pygame event passed to it by the controll class'''
+        raise NotImplementedError (f"No event handling method for {self.__class__.__name__} state")
