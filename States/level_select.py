8f553981ade0c476e1d585aa61dcc3139c81234e
Nat-133 <38531548+Nat-133@users.noreply.github.com>
2018-06-07 21:50:29 +0100

    Add files via upload
    added getLevels()

--- States/level_select.py
+++ States/level_select.py
@@ -1,8 +1,9 @@
-import os,sys
+import os, sys
 import pygame
 import pickle
 
 import template.template as template
+
 sys.path.append(os.path.relpath(".."))
 
 from Misc import button
@@ -12,10 +13,12 @@ class levelSelect(template.State):
     
     def __init__(self, screen, identifier):
         super().__init__(screen, identifier)
-
         
-        #buttonNum =
-        self.pages = Pages(self.screen, 4, 4, buttonNum, 20, 200)
+        self.pages = self.generatePages()
+    
+    def generatePages(self, screenWidth, screenHeight, rowNum, columnNum,
+                      xBorder, yBorder):
+        return 1
         
     def getLevels(self):
         """ returns the number of levles in the levels folder """
@@ -25,16 +28,15 @@ class levelSelect(template.State):
                                          and file[6:-4].isdigit()]
         return len(levels)
 
-
-class Pages:
+class Page:
     """
     holds the items to appear on each page and stores the current page
     """
-    def __init__(self, screen, columnNum, rowNum,
-                 buttonNum, xBorder, yBorder):
+    
+    def __init__(self, screen, columnNum, rowNum, xBorder, yBorder):
         self.screen = screen
         self.screenWidth, self.screenHeight = self.screen.get_size()
-
+        
         self.rowNum = rowNum
         self.columnNum = columnNum
         
@@ -42,5 +44,17 @@ class Pages:
         self.yBorder = yBorder
         self.xSpacing = int((self.screenHeight - (self.xBorder * 2))
                             / (self.columnNum - 1))
-        self.ySpacing = int((self.screenHeight - (self.yBorder*2))
+        self.ySpacing = int((self.screenHeight - (self.yBorder * 2))
                             / (self.rowNum - 1))
+    
+    @staticmethod
+    def getLevels(self):
+        """ returns the number of levles in the levels folder """
+        files = os.listdir(os.path.relpath("..//Levels"))
+        levels = [file for file in files if file[-4:] == ".txt"
+                                            and file[:6] == "level_"
+                                            and file[6:-4].isdigit()]
+        return len(levels)
+
+        
+
